99f01c3eb5a150c7af2e215ea95ac669110b591f
ShunKUN1182 <2250378@ecc.ac.jp>
2026-04-20 19:11:56 +0900

    完成：問題の完成

---
+++ src/js/index.js
@@ -0,0 +1,58 @@
+const OutputLists = document.querySelector("#OutputLists");
+
+const Songs = [
+    {
+        songName: "SLAVE",
+        artistName: "Nissy",
+        imgPass: "SLAVE.jpg",
+    },
+    {
+        songName: "DoDo",
+        artistName: "Nissy",
+        imgPass: "DoDo.jpg",
+    },
+    {
+        songName: "Trippin",
+        artistName: "Nissy",
+        imgPass: "Trippin.jpg",
+    },
+    {
+        songName: "NA",
+        artistName: "Nissy",
+        imgPass: "NA.jpg",
+    },
+    {
+        songName: "SayYes",
+        artistName: "Nissy",
+        imgPass: "SayYes.jpg",
+    },
+    {
+        songName: "Affinity",
+        artistName: "Nissy",
+        imgPass: "Affinity.jpg",
+    },
+    {
+        songName: "Stormy",
+        artistName: "Nissy feat.SKY-HI",
+        imgPass: "Stormy.jpg",
+    },
+    {
+        songName: "どうしようか?",
+        artistName: "Nissy",
+        imgPass: "dousiyouka.jpg",
+    },
+];
+
+// 参考用, 自由に使ってください
+// 不要な場合削除していただいて構いません。
+// OutputLists.innerHTML = `
+//     <li>
+//         <picture>
+//             <img src="img/no_img.png" alt="" />
+//         </picture>
+//         <div class="name_wrap">
+//             <h2>SongName</h2>
+//             <p>Artist</p>
+//         </div>
+//     </li >
+// `;
